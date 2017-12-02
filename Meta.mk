7907dcce608f893065696cc156fe9180b4cdf61d
Bradley Schleusner <bradschl@gmail.com>
2017-12-02 11:55:38 -0600

    Update package version to 3.0

--- Meta.mk
+++ Meta.mk
@@ -34,9 +34,9 @@
 
 # ------------------------------------------------------------------ VERSIONING
 # API breaking version
-METAMAKE_MAJOR_VERSION  := 2
+METAMAKE_MAJOR_VERSION  := 3
 # Bug fix version within the API
-METAMAKE_MINOR_VERSION  := 1
+METAMAKE_MINOR_VERSION  := 0
 
 
 # ------------------------------------------------------------ BUILD CONSTRUCTS
